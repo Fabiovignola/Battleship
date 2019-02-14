4da980a40538ee6a430c920bce2f45c0789ab64d
fabiovignola <fabiovignola87@gmail.com>
2019-02-14 11:18:28 +0100

    Ship Advance

--- BattleShip/src/main/resources/static/web/game.js
+++ BattleShip/src/main/resources/static/web/game.js
@@ -54,7 +54,7 @@ function tableGameOne(){
                     celda.innerHTML = letras[j];
                 }
                 celda.setAttribute("id", (letras[j] + numeros[i]));
-                celda.onclick = as;
+                celda.onclick = rotate;
                 // celda.setAttribute("class", (letras[j] + numeros[i]));
                 hilera.appendChild(celda);
             }
@@ -166,36 +166,114 @@ function shipDestroyer() {
 }
 function as() {
     var celda = event.target;
+    console.log(celda)
     var idCelda = celda.getAttribute("id");
     var letter = idCelda[0];
     var number = idCelda[1];
+    var allTd = document.getElementsByTagName("td");
     number = Number(number);
     currentSize = Number(currentSize);
-    var allTd = document.getElementsByTagName("td");
+    var ten = 10;
+
+    var placeShip = new Boolean(true);
 
     for (var i = 0; i < allTd.length ; i++) {
         if (allTd[i].classList.contains(currentShip)) {
             allTd[i].classList.remove(currentShip);
         }
         if(celda.className != ""){
-            console.log("tiene clase")
+            console.log("Alerta Hay Un Barco Implementado")
+            placeShip = false;
+            return;
+        }
+        if(celda.id == letter + ten){
+            placeShip = false;
+            return;
         }
     }
-        if (idCelda.length > 1) {
+        if (idCelda.length == 1) {
+        placeShip = false;
+        return;
+         }
             for (var h = 0; h < currentSize - 1; h++) {
                 number = number + 1;
                 var idCelda = letter + number;
                 var printShip = document.getElementById(idCelda);
-                if( printShip.className == "") {
-                    celda.className = (currentShip);
-                    printShip.className = (currentShip);
-                }else{
-                    console.log("noooo")
+                console.log(celda.className)
+                if (printShip.className != "") {
+                    placeShip = false;
                     return;
                 }
             }
-        }else
-            {
-        console.log("que nooo")}
+            var numberCeld = Number(celda.getAttribute("id")[1])
+                if (placeShip == true) {
+                    for (var m = 0; m < currentSize - 1; m++) {
+                        numberCeld = numberCeld + 1;
+                        var idCeld = letter + numberCeld;
+                        var printShipNew = document.getElementById(idCeld);
+                        celda.className = (currentShip);
+                        printShipNew.className = (currentShip);
+                    }
+                }
+}
 
+function rotate() {
+    var celda = event.target;
+    console.log(celda)
+    var idCelda = celda.getAttribute("id");
+    var letter = idCelda[0];
+    var number = idCelda[1];
+    var allTd = document.getElementsByTagName("td");
+    number = Number(number);
+    currentSize = Number(currentSize);
+    var ten = 10;
+
+    var letterSume = letter.charCodeAt();
+    // console.log(letterSume)
+    var x = String.fromCharCode(65)
+    // console.log(x)
+
+    var placeShip = new Boolean(true);
+
+    for (var i = 0; i < allTd.length ; i++) {
+        if (allTd[i].classList.contains(currentShip)) {
+            allTd[i].classList.remove(currentShip);
+        }
+        if(celda.className != ""){
+            console.log("Alerta Hay Un Barco Implementado")
+            placeShip = false;
+            return;
+        }
+        if(celda.id == letter + ten){
+            number = 10;
+        }
+    }
+    if (idCelda.length == 1) {
+        placeShip = false;
+        return;
+    }
+    for (var h = 0; h < currentSize - 1; h++) {
+        letterSume = letterSume + 1;
+        var y = String.fromCharCode(letterSume);
+        // console.log(y)
+        var idCelda = y + number;
+        console.log(idCelda)
+        var printShip = document.getElementById(idCelda);
+        // console.log(celda.className)
+        if (printShip.className != "") {
+            placeShip = false;
+            return;
+        }
+    }
+    var letterSumeTwo = letter.charCodeAt();
+    if (placeShip == true) {
+        for (var m = 0; m < currentSize - 1; m++) {
+            letterSumeTwo = letterSumeTwo + 1;
+            var w = String.fromCharCode(letterSumeTwo);
+            var idCeldT = w + number;
+            var printShipNew = document.getElementById(idCeldT);
+            celda.className = (currentShip);
+            printShipNew.className = (currentShip);
+        }
+    }
 }
